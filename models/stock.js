731c1badf95f2ddbf6bea4659a1ca54b171b8c9e
Vinicius Leal Alves <leealalves@hotmail.com>
2024-05-13 22:40:20 -0300

    feature: crud do modelo stock

--- models/stock.js
+++ models/stock.js
@@ -4,7 +4,12 @@ const stock = new mongoose.Schema({
     stockProduct: String,
     stockAmount: Number,
     stockCapacity: Number,
-    stockSpendByDay: Number
+    stockSpendByDay: Number,
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company', //id da empresa
+        require: true
+    } 
 });
 
 export default stock;
