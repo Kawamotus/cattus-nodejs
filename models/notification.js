bcd014a3f3f82009ee1b67ad4dede8cf67f0a265
Vinicius Leal Alves <leealalves@hotmail.com>
2024-05-17 22:25:01 -0300

    refactor: altera alguns atributos dos modelos camera e notification

--- models/notification.js
+++ models/notification.js
@@ -2,9 +2,27 @@ import mongoose from "mongoose";
 
 const notification = new mongoose.Schema({
     notificationDate: Date,
-    notificationTarget: String, //empresa, usuário, depende do caso
-    notificationOrigin: String, //insumos, pets, funcionarios, alguma coisa
-    notificationStatus: Boolean, //true: read, false: not read
+    notificationTarget: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'Targets',
+        require: true
+    },
+    Targets: {
+        type: String,
+        required: true,
+        enum: ['Company', 'Employee']
+    },
+    notificationOrigin: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'Origin',
+        require: true
+    },
+    Origin: {
+        type: String,
+        required: true,
+        enum: ['Stock', 'Animal']
+    },
+    notificationStatus: Boolean,
     notificationDescription: String
 });
 
