ef055a8025904499818be965bbdd016e0714b758
Vinicius Leal Alves <leealalves@hotmail.com>
2024-05-22 20:44:58 -0300

    feature: rota camera/search adicionada

--- controllers/CameraController.js
+++ controllers/CameraController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import middlewares from "../middlewares/middlewares.js"
+import utils from "../utils/utils.js";
 
 import CameraServices from "../services/CameraServices.js";
 import Camera from "../models/camera.js";
@@ -11,14 +12,14 @@ router.post("/create", middlewares.checkNecessaryFields(Camera), (req, res) => {
 
     const operation = CameraServices.Create(data)
     operation.then(result => {
-        res.send({
+        res.status(201).send({
             ok: true,
             message: "Câmera registrada com sucesso.",
             _id: result._id
         });
     }).catch(error => {
         console.log(error);
-        res.send({
+        res.status(400).send({
             message: "Erro ao registrar a câmera.",
         });
     })
@@ -34,7 +35,7 @@ router.get("/select-all/:company_id", (req, res) => {
         });
     }).catch(error => {
         console.log(error);
-        res.send({
+        res.status(400).send({
             message: "Erro ao listar as câmeras.",
         });
     })
@@ -50,7 +51,7 @@ router.get("/select-one/:camera_id", (req, res) => {
         });
     }).catch(error => {
         console.log(error);
-        res.send({
+        res.status(400).send({
             message: "Erro ao listar a câmera.",
         });
     })
@@ -60,16 +61,30 @@ router.get("/delete/:camera_id", (req, res) => {
     const operation = CameraServices.Delete(req.params.camera_id)
 
     operation.then(result => {
-        res.send({
+        res.status(204).send({
             ok: true,
-            result
         });
     }).catch(error => {
         console.log(error);
-        res.send({
+        res.status(400).send({
             message: "Erro ao deletar a câmera.",
         });
     })
+})
+
+router.post("/search", async (req, res) => {
+    const query = req.body.query
+    const fields = req.body.fields
+    const company = req.session.user.company
+
+    try {
+        const searchQuery = utils.generateSearchQuery(query, fields)
+        const operation = await CameraServices.SelectAllByFields(company, searchQuery)
+        res.send(operation)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({message: "Erro interno no servidor."})
+    }
 })
 
 export default router;
