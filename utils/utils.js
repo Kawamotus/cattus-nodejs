33a9ff19c4ce4d4b982efc4e7292282d8b3e2ecd
Vinicius Leal Alves <leealalves@hotmail.com>
2024-06-02 16:37:47 -0300

    feature: novo utils e codigo identado

--- utils/utils.js
+++ utils/utils.js
@@ -5,77 +5,94 @@ import AWS from "@aws-sdk/client-s3"
 dotenv.config()
 
 const s3 = new AWS.S3Client({
-	credentials: {
-		accessKeyId: process.env.ACCESS_KEY,
-		secretAccessKey: process.env.SECRET_ACCESS_KEY,
-	},
-	region: process.env.BUCKET_REGION
+    credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    },
+    region: process.env.BUCKET_REGION
 })
 
 class Utils {
-	generateToken(employee) {
-		return jwt.sign({
-			id: employee._id,
-			name: employee.employeeName,
-			picture: employee.employeePicture,
-			accessLevel: employee.employeeAccessLevel,
+    generateToken(employee) {
+        return jwt.sign({
+            id: employee._id,
+            name: employee.employeeName,
+            picture: employee.employeePicture,
+            accessLevel: employee.employeeAccessLevel,
             company: employee.company._id
-		}, "gatinhos", { expiresIn: 3600 })
-	}
+        }, "gatinhos", { expiresIn: 3600 })
+    }
 
-	generateSearchQuery(query, fields) {
-		return {
-			$or: fields.map(field => {
-				if (typeof field === 'string') {
-					return { [field]: { $regex: query, $options: 'i' } };
-				} else {
-					const [nestedField, nestedKey] = field;
-					return { [`${nestedField}.${nestedKey}`]: { $regex: query, $options: 'i' } };
-				}
-			})
-		};
-	};
+    generateSearchQuery(query, fields) {
+        return {
+            $or: fields.map(field => {
+                if (typeof field === 'string') {
+                    return { [field]: { $regex: query, $options: 'i' } };
+                } else {
+                    const [nestedField, nestedKey] = field;
+                    return { [`${nestedField}.${nestedKey}`]: { $regex: query, $options: 'i' } };
+                }
+            })
+        };
+    };
 
-	uploadPicture(file, file_name) {
-		const params = {
-			Bucket: process.env.BUCKET_NAME,
-			Key: file_name,
-			Body: file.buffer,
-			ContentType: file.mimetype
-		}
+    uploadPicture(file, file_name) {
+        const params = {
+            Bucket: process.env.BUCKET_NAME,
+            Key: file_name,
+            Body: file.buffer,
+            ContentType: file.mimetype
+        }
 
-		const command = new AWS.PutObjectCommand(params)
+        const command = new AWS.PutObjectCommand(params)
 
-		return s3.send(command)
-	}
+        return s3.send(command)
+    }
 
-	getUploadedPicture(file_name) {
-		return `${process.env.BUCKET_OBJECT_URL}/${file_name}`
-	}
+    getUploadedPicture(file_name) {
+        return `${process.env.BUCKET_OBJECT_URL}/${file_name}`
+    }
 
-	unFlatten(data) {
-		const result = {};
-		for (const i in data) {
-			const keys = i.split('.');
-			keys.reduce((acc, key, index) => {
-				return acc[key] = acc[key] || (index === keys.length - 1 ? data[i] : {});
-			}, result);
-		}
-		return result;
-	};
+    unFlatten(data) {
+        const result = {};
+        for (const i in data) {
+            const keys = i.split('.');
+            keys.reduce((acc, key, index) => {
+                return acc[key] = acc[key] || (index === keys.length - 1 ? data[i] : {});
+            }, result);
+        }
+        return result;
+    };
 
-	async imageUrlToBase64(imageUrl) {
-		try {
-			const response = await fetch(imageUrl);
-			const arrayBuffer = await response.arrayBuffer();
-			const buffer = Buffer.from(arrayBuffer);
-			const base64Image = buffer.toString('base64');
-			return `data:${response.headers.get('content-type')};base64,${base64Image}`;
-		} catch (error) {
-			console.error('Erro ao converter imagem em Base64:', error);
-			throw error;
-		}
-	};
+    async imageUrlToBase64(imageUrl) {
+        try {
+            const response = await fetch(imageUrl);
+            const arrayBuffer = await response.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+            const base64Image = buffer.toString('base64');
+            return `data:${response.headers.get('content-type')};base64,${base64Image}`;
+        } catch (error) {
+            console.error('Erro ao converter imagem em Base64:', error);
+            throw error;
+        }
+    };
+
+    buildUpdateFields(updates, prefix = '') {
+        const updateFields = {};
+
+        for (let key in updates) {
+            const value = updates[key];
+            const newKey = prefix ? `${prefix}.${key}` : key;
+
+            if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+                Object.assign(updateFields, this.buildUpdateFields(value, newKey));
+            } else {
+                updateFields[newKey] = value;
+            }
+        }
+
+        return updateFields;
+    };
 }
 
 export default new Utils();
