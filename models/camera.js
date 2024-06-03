cc3e2be94a02ec7476476b735912399e05dccb9d
Vinicius Leal Alves <leealalves@hotmail.com>
2024-06-02 22:09:09 -0300

    feature: novo atributo no modelo Camera

--- models/camera.js
+++ models/camera.js
@@ -4,11 +4,12 @@ const camera = new mongoose.Schema({
     cameraStatus: Number,
     cameraLocation: String,
     cameraDescription: String,
+    cameraPicture: String,
     company: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Company', //id da empresa de origem do pet
         require: true
-    } 
+    }
 });
 
 export default camera;
