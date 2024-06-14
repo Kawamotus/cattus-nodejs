b18b1dbead8cb2e0460c5a13dc25a1d1ad66526b
Vinicius Leal Alves <leealalves@hotmail.com>
2024-06-14 20:12:32 -0300

    feature: nova captura de notificacao com base nos status

--- config/socketHandlers.js
+++ config/socketHandlers.js
@@ -1,12 +1,14 @@
+import mongoose from "mongoose";
 import NotificationService from "../services/NotificationService.js";
 
 export default async (database, client) => {
-    const animalCollection = database.collection('activities');
+    const activitiesCollection = database.collection('activities');
     const stockCollection = database.collection('stocks');
+    const animalsCollection = database.collection('animals');
 
     {
-        const changeStreamAnimal = animalCollection.watch();
-        changeStreamAnimal.on('change', async (change) => {
+        const changeStreamActivities = activitiesCollection.watch();
+        changeStreamActivities.on('change', async (change) => {
             const activity = change.fullDocument
 
             const notification = {
@@ -57,6 +59,64 @@ export default async (database, client) => {
                 console.log("Erro ao emitir a notificação: " + error);
             }
         })
+    }
+    {
+        const company = client.handshake.query.company
+        const pipeline = [
+            {
+                $match: {  
+                    "fullDocument.petStatus.petCurrentStatus": {$in: ["1", "2"]}
+                }
+            },
+            {
+                $match: {
+                    "fullDocument.company": new mongoose.Types.ObjectId(company)
+                }
+            },
+            {
+                $project: {
+                    "fullDocument._id": 1, "fullDocument.petName": 1, "fullDocument.petStatus.petCurrentStatus": 1
+                }
+            }
+        ]
+        
+        const whatStatusIs = (status) => {
+            switch (status) {
+                case "0":
+                    return "Saudável"
+                case "1":
+                    return "Alerta"
+                case "2":
+                    return "Critíco"    
+                default:
+                    "Não especificado"
+            }
+        }
+
+        const changeStreamAnimals = animalsCollection.watch(pipeline, {fullDocument: 'updateLookup'})
+        changeStreamAnimals.on('change', async change => {
+            const animal = change.fullDocument
+            console.log(animal);
+            const notification = {
+                notificationDate: Date.now(),
+                notificationTarget: company,
+                Targets: "Company",
+                notificationOrigin: animal._id,
+                Origin: "Animal",
+                notificationStatus: false,
+                notificationDescription: `Status do animal ${animal.petName} mudou para ${whatStatusIs(animal.petStatus.petCurrentStatus)}.`
+            }
+            try {
+                const { _id } = await NotificationService.Create(notification)
+                const newNotification = await NotificationService.SelectOne(_id.toHexString())
+                client.emit('notificationStatus', newNotification);
+
+            } catch (error) {
+                console.log("Erro ao emitir a notificação: " + error);
+            }
+        })
+
+
     }
     client.on("disconnect", async () => {
         console.log("Cattus WEB desconectado.");
