186461927ad87de899942515725b4af69077c327
Gabrigom <vinny.gab18@gmail.com>
2025-05-18 14:09:00 -0300

    feat: fetch de gatos por cameraId

--- controllers/ActivityController.js
+++ controllers/ActivityController.js
@@ -1,87 +1,120 @@
-import express from "express";
-import middlewares from "../middlewares/middlewares.js";
+import express from 'express';
+import middlewares from '../middlewares/middlewares.js';
 
-import ActivityServices from "../services/ActivityServices.js";
-import Activity from "../models/activity.js";
+import ActivityServices from '../services/ActivityServices.js';
+import Activity from '../models/activity.js';
 
 const router = express.Router();
 
-router.post("/create", middlewares.checkNecessaryFields(Activity), (req, res) => {
-    const data = req.body
+router.post(
+  '/create',
+  middlewares.checkNecessaryFields(Activity),
+  (req, res) => {
+    const data = req.body;
 
-    const operation = ActivityServices.Create(data)
-    operation.then(result => {
+    const operation = ActivityServices.Create(data);
+    operation
+      .then((result) => {
         res.status(201).send({
-            ok: true,
-            message: "Atividade registrada com sucesso.",
-            _id: result._id
+          ok: true,
+          message: 'Atividade registrada com sucesso.',
+          _id: result._id,
         });
-    }).catch(error => {
+      })
+      .catch((error) => {
         console.log(error);
         res.status(400).send({
-            message: "Erro ao registrar atividade.",
+          message: 'Erro ao registrar atividade.',
         });
+      });
+  },
+);
+
+router.get('/select-all/:author_id', (req, res) => {
+  const operation = ActivityServices.SelectAll(req.params.author_id);
+
+  operation
+    .then((result) => {
+      res.send({
+        ok: true,
+        result,
+      });
     })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({
+        message: 'Erro ao listar as atividades.',
+      });
+    });
 });
 
-router.get("/select-all/:author_id", (req, res) => {
-    const operation = ActivityServices.SelectAll(req.params.author_id)
+router.get('/select-one/:activity_id', (req, res) => {
+  const operation = ActivityServices.SelectOne(req.params.activity_id);
 
-    operation.then(result => {
-        res.send({
-            ok: true,
-            result
-        });
-    }).catch(error => {
-        console.log(error);
-        res.status(400).send({
-            message: "Erro ao listar as atividades.",
-        });
+  operation
+    .then((result) => {
+      res.send({
+        ok: true,
+        result,
+      });
     })
-})
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({
+        message: 'Erro ao listar a atividade.',
+      });
+    });
+});
 
-router.get("/select-one/:activity_id", (req, res) => {
-    const operation = ActivityServices.SelectOne(req.params.activity_id)
+router.delete('/delete/:activity_id', (req, res) => {
+  const operation = ActivityServices.Delete(req.params.activity_id);
 
-    operation.then(result => {
-        res.send({
-            ok: true,
-            result
-        });
-    }).catch(error => {
-        console.log(error);
-        res.status(400).send({
-            message: "Erro ao listar a atividade.",
-        });
+  operation
+    .then((result) => {
+      res.status(204).send({
+        ok: true,
+      });
     })
-})
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({
+        message: 'Erro ao deletar a atividade.',
+      });
+    });
+});
 
-router.delete("/delete/:activity_id", (req, res) => {
-    const operation = ActivityServices.Delete(req.params.activity_id)
+router.get('/charts/average-animal-activity/:interval', async (req, res) => {
+  const company = req.session.user.company;
+  const interval = req.params.interval;
 
-    operation.then(result => {
-        res.status(204).send({
-            ok: true,
-        });
-    }).catch(error => {
-        console.log(error);
-        res.status(400).send({
-            message: "Erro ao deletar a atividade.",
-        });
+  try {
+    const [result] = await ActivityServices.SelectAverageActivitiesTime(
+      company,
+      interval,
+    );
+    res.send({ intervalo: interval, result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: 'Erro interno no servidor.' });
+  }
+});
+
+router.get('/select-by-camera/:camera_id', (req, res) => {
+  const operation = ActivityServices.SelectByCameraId(req.params.camera_id);
+
+  operation
+    .then((result) => {
+      res.send({
+        ok: true,
+        result,
+      });
     })
-})
-
-router.get("/charts/average-animal-activity/:interval", async (req, res) => {
-    const company = req.session.user.company
-    const interval = req.params.interval
-
-    try {
-        const [result] = await ActivityServices.SelectAverageActivitiesTime(company, interval)
-        res.send({intervalo: interval, result})
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Erro interno no servidor." })
-    }
-})
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({
+        message: 'Erro ao listar as atividades dessa câmera.',
+      });
+    });
+});
 
 export default router;
--- services/ActivityServices.js
+++ services/ActivityServices.js
@@ -1,38 +1,45 @@
-import mongoose from "mongoose";
-import activity from "../models/activity.js";
-import utils from "../utils/utils.js";
+import mongoose from 'mongoose';
+import activity from '../models/activity.js';
+import utils from '../utils/utils.js';
 
-const Activity = mongoose.model("activity", activity);
+const Activity = mongoose.model('activity', activity);
 
 class ActivityServices {
+  SelectAll(id) {
+    return Activity.find({ activityAuthor: id })
+      .populate('activityAuthor')
+      .populate('activityCameraAuthor');
+  }
 
-    SelectAll(id) {
-        return Activity.find({ activityAuthor: id }).populate("activityAuthor").populate("activityCameraAuthor");
-    }
+  SelectAllNoCriteria() {
+    return Activity.find();
+  }
 
-    SelectAllNoCriteria() {
-        return Activity.find();
-    }
+  SelectAverageActivitiesTime(company, interval) {
+    company = new mongoose.Types.ObjectId(company);
+    return Activity.aggregate(
+      utils.pipelineAverageActivitiesTime(company, interval),
+    );
+  }
 
-    SelectAverageActivitiesTime(company, interval) {
-        company = new mongoose.Types.ObjectId(company)
-        return Activity.aggregate(utils.pipelineAverageActivitiesTime(company, interval))
+  SelectOne(id) {
+    return Activity.findById(id).populate('activityAuthor');
+  }
 
-    }
+  Create(data) {
+    const newActivity = new Activity(data);
+    return newActivity.save();
+  }
 
-    SelectOne(id) {
-        return Activity.findById(id).populate("activityAuthor");
-    }
+  Delete(id) {
+    return Activity.findByIdAndDelete(id);
+  }
 
-    Create(data) {
-        const newActivity = new Activity(data);
-        return newActivity.save()
-    }
-
-    Delete(id) {
-        return Activity.findByIdAndDelete(id)
-    }
-
+  SelectByCameraId(cameraId) {
+    return Activity.find({ activityCameraAuthor: cameraId })
+      .populate('activityAuthor')
+      .populate('activityCameraAuthor');
+  }
 }
 
 export default new ActivityServices();
